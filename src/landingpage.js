b313507b30f2659180464dcde186704f83770c67
Suyash30 <91048475+JavaPanda30@users.noreply.github.com>
2023-10-12 18:58:20 +0530

    Update landingpage.js

--- src/landingpage.js
+++ src/landingpage.js
@@ -1,29 +1,33 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react"; // Import React and useState
 import Axios from "axios";
 
 function LandingPage() {
-    const[data,setData] = usedState([]);
+  const [data, setData] = useState([]); // Corrected useState syntax
+
   useEffect(() => {
     Axios.get("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
         if (res.status === 200) {
           setData(res.data);
         } else {
-          Promise.reject();
+          return Promise.reject(); // Corrected the rejection
         }
       })
       .catch((err) => alert(err));
   }, []);
-  ListName=()=>{
-    return data.map(val){
-        render(<p>val</p>);
-    };
-  }
+
+  const ListName = () => { // Added 'const' to define the function
+    return data.map((val) => ( // Corrected function definition and mapping
+      <p key={val.id}>{val.name}</p> // Added a 'key' prop for each list item
+    ));
+  };
+
   return (
     <div>
       <h1>Landing Page</h1>
-      {ListName();}
+      {ListName()}
     </div>
   );
 }
+
 export default LandingPage;
